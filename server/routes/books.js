19713272921df66f0e48d2412e4e98e671db403b
Ayman Alqaq <aalqaq2@yahoo.com>
2021-04-01 15:17:40 -0500

    Implementing Create

--- server/routes/books.js
+++ server/routes/books.js
@@ -5,23 +5,16 @@ const connectionPool = require('../database/connection-pool');
 
 router.post('/', function (req, res) {
   console.log('POST BODY', req.body);
-})
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-
-  const book = {
-    'author': 'Charles Dickens',
-    'title': 'Great Expectations',
-    'published': '1861-01-01'
-  }
-
-  connectionPool.getPool().query('insert into library set ?', book, (err, result) => {
+  connectionPool.getPool().query('insert into library set ?', req.body, (err, result) => {
     if (err) throw err;
 
     console.log(result);
   });
+})
 
+/* GET users listing. */
+router.get('/', function (req, res) {
   res.send('Books found here.');
 });
 
