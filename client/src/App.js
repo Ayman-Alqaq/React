e335c06048ca7a3225ff2fc8af1c2129570688dc
Ayman Alqaq <aalqaq2@yahoo.com>
2021-04-03 22:00:24 -0500

    Using React Router - Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Header from './Header';
 import BookLibrary from './BookLibrary';
+import Book from './Book';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <BookLibrary />
-    </div>
+    <Router>
+
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">A
+            <BookLibrary />
+          </Route>
+          <Route path="/create">
+            <Book />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
